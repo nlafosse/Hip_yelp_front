fc60892a76d282a298117f8208cd0a8bf02c1555
hengyuni <hengyuni@gmail.com>
2021-04-22 15:44:21 -0400

    details

--- src/Components/FoodDetail/FoodDetail.js
+++ src/Components/FoodDetail/FoodDetail.js
@@ -23,39 +23,3 @@ export default function FoodDetail ({ hotspot }) {
     </div>
   )
 }
-
-// const Food = (props) => {
-//   const {match} = props;
-//   const [params] = match;
-//   const {foodId} = params;
-
-//   const [foodData, setFoodData] = useState(mockDataFoods)
-
-//   const getFoodData = async () => {
-//     try {
-//       const res = await fetch("https://hotspot1.herokuapp.com/foods/" + foodId)
-//       const data = await res.json();
-//       setFoodData(data)
-//     } catch (err) {
-//       console.log(err)
-//     }
-//   }
-
-//   useEffect(() => {
-//     getFoodData();
-//   }, [])
-
-//   // console.log(foodData)
-
-//   return (
-//     <div className= "Foods">
-//       <img src={foodData.photo_url} className="food_url" width="150px"/>
-//       <p>{foodData.name}</p>
-//       <p>type: {foodData.group}</p>
-//       <p>address: {foodData.address}</p>
-//       <p>description: {foodData.description}</p>
-//     </div>
-//   )
-// }
-
-// export default Food;
