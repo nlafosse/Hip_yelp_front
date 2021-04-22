3ce5037f0d6bc3c05e898d5dbc145a25e00220ad
tjconti12 <tjconti12@gmail.com>
2021-04-22 12:30:20 -0400

    added images file. Basic styling to home page using css module

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,31 @@
+import {Link} from 'react-router-dom'
+import placeholder from '../../Images/placeholder.png'
+import * as homeStyles from './Home.module.css'
 
 const Home = () => {
     return (
         <div>
-            
+            <h1 className={homeStyles.title}>Test Title</h1>
+            <div className={homeStyles.container}>
+                <div>
+                    <Link to="">
+                        <h2>Food</h2>
+                        <div className={homeStyles.imgContainer}>
+                            <img src={placeholder} />
+                        </div>
+                    </Link>
+                </div>
+                <div>
+                    <Link to="">
+                        <h2>Drinks</h2>
+                        <div className={homeStyles.imgContainer}>
+                            <img src={placeholder} />
+                        </div>
+                    </Link>
+                </div>
+            </div>
         </div>
+        
     )
 }
 
