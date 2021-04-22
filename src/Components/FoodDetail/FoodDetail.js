e6f88250b1e27cf86cc846a3bc5098182590614f
hengyuni <hengyuni@gmail.com>
2021-04-22 16:44:41 -0400

    food details up

--- src/Components/FoodDetail/FoodDetail.js
+++ src/Components/FoodDetail/FoodDetail.js
@@ -1,30 +1,26 @@
 import React from "react";
 import { Link } from 'react-router-dom'
 
-export default function FoodDetail ({routerProps}) {
-  console.log(routerProps)
-  // if ( === undefined) {
-  //   return <h1>Loading...</h1>
-  // }
 
-  const selected = [...routerProps].filter(
-    (h) => h.id === routerProps.match.params.id
+export default function FoodDetail ({routerProps, hotspot}) {
+  console.log(routerProps)
+
+  const selected = [...hotspot].filter(
+    (h) => h.id == routerProps.match.params.id
   );
 
   console.log(selected)
 
   return (
     <div>
-      <h1>test</h1>
+      <img src={selected[0].photo_url} className="food_url" width="40px"/>
       <h1>{selected[0].name}</h1>
-      {/* <img src={getHotSpot.photo_url} className="food_url" width="150px"/>
-      <h1>{getHotSpot.name}</h1>
-      <p>{getHotSpot.description}</p>
-      <p>Type: {getHotSpot.group}</p>
-      <p>Address: {getHotSpot.address}</p>
+      <p>{selected[0].description}</p>
+      <p>Type: {selected[0].group}</p>
+      <p>Address: {selected[0].address}</p>
       <div>
         <Link to="/">Home</Link>
-      </div> */}
+      </div>
     </div>
   )
 }
