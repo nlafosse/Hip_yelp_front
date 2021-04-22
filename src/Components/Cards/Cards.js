d6b544f69224754df8c200f32c45a69882cbbdd7
buggydev <mrtyreeharper@gmail.com>
2021-04-22 11:01:36 -0700

    basic cards done Css pending

--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -1,18 +1,21 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+// import './Cards.css';
 
-
-export default function Cards ({placeData}) {
+export default function Cards ({ placeData , group }) {
     // if image_url is null us place holder image
-
+    return(
+        <div className="cards">
     {placeData.map((place,i)=> {
         return(
                 <div key={i}>
-                <Link to={`/foods/${place.id}`}>
-                <img src={place.photo_url} alt={"image"}  onError={(event) => {
+                <Link to={`/${group}/${place.id}`}>
+                <img  src={place.photo_url} alt={"image"}  onError={(event) => {
                     event.target.src = '' }} />
-                    <h2>{place.name}</h2>         
+                    <h2 className="cards_text">{place.name}</h2>         
                 </Link>
                 </div>
             )
     })}
+    </div>
+    )
 }
