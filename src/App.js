5e84094d9e1e3d935d0e6ce09c31788ab894c00b
buggydev <mrtyreeharper@gmail.com>
2021-04-22 14:05:54 -0700

    test

--- src/App.js
+++ src/App.js
@@ -44,9 +44,9 @@ export default function App () {
     }
   }
   // We dont need to pull the data on page load
-  // useEffect(() => {
-  //   getApiDate();
-  // }, []);
+  useEffect(() => {
+    getApiData();
+  }, []);
 
 
 
