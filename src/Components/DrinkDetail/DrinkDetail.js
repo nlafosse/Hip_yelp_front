071fb96e032e1bef9ae869a0b4c3bc8da61ad52b
hengyuni <hengyuni@gmail.com>
2021-04-22 17:02:59 -0400

    drink details

---
+++ src/Components/DrinkDetail/DrinkDetail.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from 'react-router-dom'
+
+export default function DrinkDetail ({routerProps, hotspot}) {
+  console.log(routerProps)
+
+  const selected = [...hotspot].filter(
+    (h) => h.id == routerProps.match.params.id
+  );
+
+  console.log(selected)
+
+  return (
+    <div>
+      <img src={selected[0].photo_url} className="food_url" width="40px"/>
+      <h1>{selected[0].name}</h1>
+      <p>{selected[0].description}</p>
+      <p>Type: {selected[0].group}</p>
+      <p>Address: {selected[0].address}</p>
+      <div>
+        <Link to="/">Home</Link>
+      </div>
+    </div>
+  )
+}
